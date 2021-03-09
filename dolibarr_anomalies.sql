5d3c585164841ad1d2c714f0239b18e965cc6b05
epehj <dev@epehj.com>
2021-03-09 23:32:39 +0100

    parametres de dev

--- dolibarr_anomalies.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO dolibarr.aw_anomalie (label) VALUES
-	 ('Prise de Rendez vous'),
-	 ('Cave inax'),
-	 ('Clés à récupérer'),
-	 ('Horaires particuliers'),
- 	 ('Faro nécessaire'),
-	 ('Erreur contact + accès');
--- dolibarr_type_batiment.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO dolibarr.aw_type_batiment (`type`) VALUES
-	 ('Bateau'),
-	 ('Chambre'),
-	 ('École'),
-	 ('ERP'),
-	 ('Habitation'),
-	 ('Hôpital'),
-	 ('Hôtel'),
-	 ('Parking');
